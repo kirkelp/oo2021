9986e98c2770524570dd18b87e2845197678ab41
Kirkelp <kirkelp@tlu.ee>
2021-05-07 14:37:12 +0300

    pausilt jätkan

--- w13_kontrolltoo2/Kellamehhanism/Main.java
+++ w13_kontrolltoo2/Kellamehhanism/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] arg) {
     Pendel pendel = new Pendel(0.7, 6);
 
-    System.out.println(pendel);
+    System.out.println(v6nkeperiood);
     }
 }
