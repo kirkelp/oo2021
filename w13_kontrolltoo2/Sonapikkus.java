c0d0dc98baa21f953f87837a00e945acce752332
Kirkelp <kirkelp@tlu.ee>
2021-05-07 16:01:04 +0300

    16.00

--- w13_kontrolltoo2/Sonapikkus.java
+++ w13_kontrolltoo2/Sonapikkus.java
@@ -1,9 +1,15 @@
+//Sõnapikkuste uuring
+// Salvesta uurimiseks vähemalt paarileheküljeline tekst.
+// * Koosta programm, mis loeks kokku, mitu kolmetähelist sõna on tekstis.
+// * Koosta graafik, kus ühel teljel on teksti algusest sinnamaani esinenud kolmetäheliste sõnade arv ning teisel teljel selleks ajaks leitud kolmetäheliste erinevate sõnade arv.
+// * Võta uuringuks tunduvalt pikem tekst. Kuva joonisele sarnased graafikud 1-10 tähe pikkuste sõnade kohta. Mummukestega tähista joonistele kohad, kus on läbitud veerand, pool ja kolmveerand tekstist.
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Proov {
+public class Sonapikkus {
     public static void main(String[] args) throws IOException{
         File file = new File ("tekst.txt");
         FileReader fr = new FileReader(file);
